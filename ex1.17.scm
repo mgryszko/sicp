8ecb5a852d21e82fa1c986c8d142303a3ebbf3de
Marcin Gryszko <marcin@grysz.com>
2012-04-18 21:56:10 +0200

    1.2.4 completed

---
+++ ex1.17.scm
@@ -0,0 +1,18 @@
+(define (even? n)
+  (= (remainder n 2) 0))
+
+(define (halve n)
+  (/ n 2))
+
+(define (double n)
+  (+ n n))
+
+(define (* a b)
+  (cond ((= b 0) 0)
+	((even? b) (* (double a) (halve b)))
+	(else (+ a (* a (- b 1))))))
+      
+(* 2 2)
+(* 3 4)
+(* 15 11)
+
